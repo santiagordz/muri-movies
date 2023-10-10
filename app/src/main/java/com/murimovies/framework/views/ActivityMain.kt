173270d998d1b4c55c92cec2f6e96349629cf37b
santiagordz <santiromur@gmail.com>
2023-10-10 11:57:21 -0600

    add layout

---
+++ app/src/main/java/com/murimovies/framework/views/ActivityMain.kt
@@ -0,0 +1,17 @@
+package com.murimovies.framework.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.murimovies.databinding.ActivityMainBinding
+
+class ActivityMain: AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        initializeBinding()
+//        initializeObservers()
+//        initializeListeners()
+    }
+
+}
